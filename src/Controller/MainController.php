6fe44b43301de15d360ed4b8893847c10ca17a53
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-12-29 12:46:48 +0100

    some template inheritance and basic html printing

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
-    public function homePgae(): Response
+    public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: something that is supposed to make sense');
+        $starShipCount = 456;
+
+        $myShip = [
+            'name' => 'something something',
+            'class' => 'garden',
+            'captain' => 'jean-luc',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarShips' => $starShipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
